6c655e18f1654c1966b809a33db4581486cef0e5
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-24 14:22:56 -0300

    Get all surveys

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ const userController = new UserController()
 const surveyController = new SurveyController()
 
 router.post("/users", userController.create)
+
+router.get("/surveys", surveyController.show)
 router.post("/surveys", surveyController.create)
 
 export { router }
